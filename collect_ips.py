e47447c43fcaf84642d75edd9d29d81c79583aee
jmqoeu <jmqoeu@gmail.com>
2025-05-03 03:32:18 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -32,7 +32,7 @@ with open('ip.txt', 'w') as file:
         elif url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
         elif url == 'https://stock.hostmonit.com/CloudFlareYes':
-            elements = soup.find_all('td')
+            elements = soup.find_all('div class=\"cell\"')
         #elif url == 'https://cf.090227.xyz':
             #elements = soup.find_all('tr')
         else:
